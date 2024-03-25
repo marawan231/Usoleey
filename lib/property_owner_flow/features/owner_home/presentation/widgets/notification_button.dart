f8aaaba0d220f1884d99679acd515dbd58708cc4
Khaled Ghorab <47598811+khaledghorab@users.noreply.github.com>
2024-03-25 22:42:17 +0200

    my real state feature

--- lib/property_owner_flow/features/owner_home/presentation/widgets/notification_button.dart
+++ lib/property_owner_flow/features/owner_home/presentation/widgets/notification_button.dart
@@ -18,8 +18,11 @@ class NotificationButton extends StatelessWidget {
             badgeContent: state.getHomeStatsState != RequestState.loading
                 ? Text(state.homeModel!.notificationsCount.toString(),
                     style: getBoldStyle(fontSize: 11, color: Colors.white))
-                : CupertinoActivityIndicator(),
+                : CupertinoActivityIndicator(
+                    radius: 3,
+                  ),
             position: badges.BadgePosition.topStart(start: -3),
+            showBadge: state.homeModel!.notificationsCount! > 0,
             child: InkWell(
                 onTap: () => Go.toNamed(NamedRoutes.notifications),
                 child: SvgPicture.asset(AssetsManager.ownerBell)),
